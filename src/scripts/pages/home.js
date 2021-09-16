a242e2354424bb5c46baa9745bf00bf44de00899
Dallas Hogan <dhogan@maxmind.com>
2021-09-16 13:48:39 -0400

    changed toggle form to button

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const $visibleCategoriesForm = document.querySelector('.categories-toggle__form');
   const $publicCategoriesSection = document.querySelector('.categories-section--public');
   const $privateCategoriesSection = document.querySelector('.categories-section--private');
+  const $toggleBtn = document.querySelector('.categories-toggle-btn');
 
-  const toggleVisibleCaterories = () => {
-    $publicCategoriesSection.classList.toggle('active');
-    $privateCategoriesSection.classList.toggle('active');
-  };
+  function showVisibleCategories () {
+    if ($toggleBtn.innerText === 'Switch to Private') {
+      $publicCategoriesSection.classList.remove('active');
+      $privateCategoriesSection.classList.add('active');
+      $toggleBtn.innerText = 'Switch to Public';
+    } else if ($toggleBtn.innerText === 'Switch to Public') {
+      $publicCategoriesSection.classList.add('active');
+      $privateCategoriesSection.classList.remove('active');
+      $toggleBtn.innerText = 'Switch to Private';
+    }
+  }
 
-  $visibleCategoriesForm.addEventListener('change', toggleVisibleCaterories);
+  $toggleBtn.addEventListener('click', showVisibleCategories);
 });
