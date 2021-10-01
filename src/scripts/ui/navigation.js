8675534bddfe6271dd14309efe7549b5d1a79eb3
Dallas Hogan <dhogan@maxmind.com>
2021-10-01 13:26:00 -0400

    changing search bar

--- src/scripts/ui/navigation.js
+++ src/scripts/ui/navigation.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
   const $headerCheckbox = document.getElementById('header-checkbox');
+  const $headerSearch = document.querySelector('.header-search > .search');
+  const $headerSearchBtn = document.querySelector('.search-mobile-btn');
+  const $headerSearchQuery = document.querySelector('.header-search > .search > #query');
 
   $headerCheckbox.addEventListener('click', () => {
     $headerCheckbox.classList.toggle('closed');
     $headerCheckbox.classList.toggle('open');
+  });
+
+  $headerSearchBtn.addEventListener('click', () => {
+    $headerSearch.classList.add('search-mobile-open');
+    $headerSearchQuery.focus();
   });
 });
