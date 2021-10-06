36af9bde1b63718f8c66ce5ed098f3d22fe21153
Dallas Hogan <dhogan@maxmind.com>
2021-10-06 12:33:55 -0400

    adding sessionStorage to toggle

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -3,15 +3,46 @@ document.addEventListener('DOMContentLoaded', () => {
   const $privateCategoriesSection = document.querySelector('.categories-section--private');
   const $toggleBtn = document.querySelector('.categories-toggle-btn');
 
+  const privateBtn = 'Switch to Private';
+  const publicBtn = 'Switch to Public';
+
+  function showPublicCategories () {
+    sessionStorage.setItem('toggle', 'showingPrivate');
+    $publicCategoriesSection.classList.add('active');
+    $privateCategoriesSection.classList.remove('active');
+    $toggleBtn.innerText = privateBtn;
+  }
+
+  function showPrivateCategories () {
+    sessionStorage.setItem('toggle', 'showingPublic');
+    $publicCategoriesSection.classList.remove('active');
+    $privateCategoriesSection.classList.add('active');
+    $toggleBtn.innerText = publicBtn;
+  }
+
+  if (typeof (Storage) !== 'undefined') {
+    if (sessionStorage.length === 0) {
+      sessionStorage.setItem('toggle', 'showingPublic');
+    } else {
+      if (sessionStorage.getItem('toggle') === 'showingPublic') {
+        showPublicCategories();
+      } else if (sessionStorage.getItem('toggle') === 'showingPrivate') {
+        showPrivateCategories();
+      }
+    }
+  }
+
   function showVisibleCategories () {
-    if ($toggleBtn.innerText === 'Switch to Private') {
-      $publicCategoriesSection.classList.remove('active');
-      $privateCategoriesSection.classList.add('active');
-      $toggleBtn.innerText = 'Switch to Public';
-    } else if ($toggleBtn.innerText === 'Switch to Public') {
-      $publicCategoriesSection.classList.add('active');
-      $privateCategoriesSection.classList.remove('active');
-      $toggleBtn.innerText = 'Switch to Private';
+    if ($toggleBtn.innerText === privateBtn) {
+      showPublicCategories();
+    } else if ($toggleBtn.innerText === publicBtn) {
+      showPrivateCategories();
+    }
+
+    if (sessionStorage.getItem('toggle') === 'showingPublic') {
+      showPublicCategories();
+    } else if (sessionStorage.getItem('toggle') === 'showingPrivate') {
+      showPrivateCategories();
     }
   }
 
