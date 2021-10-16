e0951f983dd521e79c59cf02b8df56f8a02ca755
Dallas <dallas@Dallass-MacBook-Pro.local>
2021-10-16 13:02:38 -0400

    fixing sidenav

--- src/scripts/ui/navigation.js
+++ src/scripts/ui/navigation.js
@@ -1,13 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
   const $headerCheckbox = document.getElementById('header-checkbox');
+  const $sidebar = document.querySelector('.header-sidebar');
+  const $body = document.querySelector('body');
+
   const $headerSearch = document.querySelector('.header-search > .search');
   const $headerSearchBtn = document.querySelector('.search-mobile-btn');
   const $headerSearchQuery = document.querySelector('.header-search > .search > #query');
 
-  $headerCheckbox.addEventListener('click', () => {
+  function headerCheckboxToggle () {
     $headerCheckbox.classList.toggle('closed');
     $headerCheckbox.classList.toggle('open');
-  });
+
+    $sidebar.style.display === 'block'
+      ? $sidebar.style.display = 'none'
+      : $sidebar.style.display = 'block';
+
+    $body.classList.contains('blur-background')
+      ? $body.classList.remove('blur-background')
+      : $body.classList.add('blur-background');
+
+    $sidebar.classList.contains('header-sidebar-hidden')
+      ? $sidebar.classList.remove('header-sidebar-hidden')
+      : $sidebar.classList.add('header-sidebar-hidden');
+  }
+
+  $headerCheckbox.addEventListener('click', headerCheckboxToggle);
 
   $headerSearchBtn.addEventListener('click', () => {
     $headerSearch.classList.add('search-mobile-open');
